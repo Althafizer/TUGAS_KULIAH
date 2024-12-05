3351d60a302cae46163c51d706b1c9645fd0dba3
Althafizer <152487917+Althafizer@users.noreply.github.com>
2024-12-05 10:11:27 +0700

    feat/tugas_praktikum_kbp_10

---
+++ Praktikum_KBP_10/script.js
@@ -0,0 +1,19 @@
+const container1 = document.getElementById("container1");
+const container2 = document.getElementById("container2");
+const list = document.querySelectorAll("li");
+const paragraph = document.querySelectorAll("p");
+const header = document.getElementById("h1");
+
+header.style.backgroundColor = "White";
+container1.style.backgroundColor = "Pink";
+container2.style.backgroundColor = "Pink";
+
+for(let i = 0; i < paragraph.length; i++){
+    paragraph[i].style.backgroundColor = 'White';
+}
+
+for(let i = 0; i < list.length; i++){
+    list[i].style.backgroundColor = 'white';
+    list[i].style.fontWeight = 'bold';
+    list[i].style.fontFamily = 'Verdana';
+}
